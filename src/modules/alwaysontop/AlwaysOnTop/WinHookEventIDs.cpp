306c35eeedd664322fd2b04db78f57fdfb63c835
Anders <sredna@gmail.com>
2023-08-02 01:34:52 +0200

    RegisterWindowMessage does not require a lock

--- src/modules/alwaysontop/AlwaysOnTop/WinHookEventIDs.cpp
+++ src/modules/alwaysontop/AlwaysOnTop/WinHookEventIDs.cpp
@@ -4,11 +4,7 @@
 
 UINT WM_PRIV_SETTINGS_CHANGED;
 
-std::once_flag init_flag;
-
 void InitializeWinhookEventIds()
 {
-    std::call_once(init_flag, [&] {
-        WM_PRIV_SETTINGS_CHANGED = RegisterWindowMessage(L"{11978F7B-221A-4E65-B8A8-693F7D6E4B25}");
-    });
+    WM_PRIV_SETTINGS_CHANGED = RegisterWindowMessage(L"{11978F7B-221A-4E65-B8A8-693F7D6E4B25}");
 }
